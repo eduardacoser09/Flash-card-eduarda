9860c705860de374d563e7f23c7d440df31d885c
eduardacoser09 <coser.eduarda@escola.pr.gov.br>
2025-09-16 09:35:58 -0300

    16/09

--- Perguntas.js
+++ Perguntas.js
@@ -5,13 +5,54 @@ criaCartao(
 )
 
 criaCartao(
-    'categoria',
+    'Conhecimentos gerais',
     'Quantas patas uma formiga possui?',
     '6'
 )
 
 criaCartao(
+    'Matemática',
+    ' Quantos anos tem um século?',
+    '100'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é o nome do único satélite natural da terra?',
+    'Lua'
+)
+criaCartao(
+    'Geografia',
+    'Qual é o maior país da América do Sul?',
+    'Brasil'
+)
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem pintou o quadro Mona Lisa?',
+    'Leonardo da Vinci'
+)
+criaCartao(
+    'Conhecimentos gerais',
+    'Que nome se da para o coletivo de lobos?',
+    'Alcateia'
+)
+    criaCartao(
+    'Geografia',
+    'Como é o nome da energia gerada pelo vento?',
+    'Eólica'
+)
+criaCartao(
+    'Química',
+    'Qual é a fórmula da água?',
+    'H2O'
+)
+criaCartao(
+    'História',
+    'Qual é o nome do famoso navio que naufragou em 1912 e foi tema de um filme?',
+    'Titanic'
+)
+criaCartao(
     'categoria',
-    'pergunta',
-    'resposta'
+    'Qual é a capital da Espanha?',
+    'Madrid'
 )
