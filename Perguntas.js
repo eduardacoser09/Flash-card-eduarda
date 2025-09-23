fce548ee7c0bd5a4f8085b3f31cceeec2226b2ff
eduardacoser09 <coser.eduarda@escola.pr.gov.br>
2025-09-23 09:31:44 -0300

    23/09

--- Perguntas.js
+++ Perguntas.js
@@ -5,48 +5,48 @@ criaCartao(
 
 
 criaCartao(
-    'Matemática',
-    ' Quantos anos tem um século?',
-    '100'
-)
+        'Matemática',
+        ' Quantos anos tem um século?',
+        '100'
+    )
 
 criaCartao(
-    'Conhecimentos gerais',
-    'Qual é o nome do único satélite natural da terra?',
-    'Lua'
-)
+        'Conhecimentos gerais',
+        'Qual é o nome do único satélite natural da terra?',
+        'Lua'
+    )
 criaCartao(
-    'Geografia',
-    'Qual é o maior país da América do Sul?',
-    'Brasil'
-)
+        'Geografia',
+        'Qual é o maior país da América do Sul?',
+        'Brasil'
+    )
 criaCartao(
-    'Conhecimentos gerais',
-    'Quem pintou o quadro Mona Lisa?',
-    'Leonardo da Vinci'
-)
+        'Conhecimentos gerais',
+        'Quem pintou o quadro Mona Lisa?',
+        'Leonardo da Vinci'
+    )
 criaCartao(
-    'Conhecimentos gerais',
-    'Que nome se da para o coletivo de lobos?',
-    'Alcateia'
-)
+        'Conhecimentos gerais',
+        'Que nome se da para o coletivo de lobos?',
+        'Alcateia'
+    )
     criaCartao(
-    'Geografia',
-    'Como é o nome da energia gerada pelo vento?',
-    'Eólica'
-)
+        'Geografia',
+        'Como é o nome da energia gerada pelo vento?',
+        'Eólica'
+    )
 criaCartao(
-    'Química',
-    'Qual é a fórmula da água?',
-    'H2O'
-)
+        'Química',
+        'Qual é a fórmula da água?',
+        'H2O'
+    )
 criaCartao(
-    'História',
-    'Qual é o nome do famoso navio que naufragou em 1912 e foi tema de um filme?',
-    'Titanic'
-)
+        'História',
+        'Qual é o nome do famoso navio que naufragou em 1912 e foi tema de um filme?',
+        'Titanic'
+    )
 criaCartao(
-    'categoria',
-    'Qual é a capital da Espanha?',
-    'Madrid'
-)
+        'categoria',
+        'Qual é a capital da Espanha?',
+        'Madrid'
+    )
