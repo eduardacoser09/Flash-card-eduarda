5ce9df14d18935dc5f98d129f6033e62146c2d48
eduardacoser09 <coser.eduarda@escola.pr.gov.br>
2025-09-23 10:33:39 -0300

    23/09

--- app.js
+++ app.js
@@ -18,8 +18,11 @@ function criaCartao(categoria, pergunta, resposta) {
 
   function viraCartao(){
     respostaEstaVisivel=!respostaEstaVisivel
+    cartao.classList.toggle('active',respostaEstaVisivel)
   }
 
+  cartao.addEventListener('click',viraCartao)
+  
   container .appendChild(cartao)
 
 }
